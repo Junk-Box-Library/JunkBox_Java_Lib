db76959d0c7407d69dec388d36e7ecf178d10bf1
Fumi.Hax <iseki@rsch.tuis.ac.jp>
2023-12-19 22:00:41 +0900

    Update UDP_S.java

--- UDP_S.java
+++ UDP_S.java
@@ -1,32 +1,26 @@
 
-
 import jp.jbxl.*;
-
-
 
 class UDP_S
 {
+    public static void  main(String args[])
+    {
+        UDP udp = new UDP(8000);
+        String recv = "";
 
-	public static void  main(String args[])
-   	{
-    	UDP udp = new UDP(8000);
-       	String recv = "";
-
-		while(true){
-       		try {
-           		recv = udp.recvMesg();
-           		udp.sendMesgln("OK");
-       		}
-       		catch (Exception er) {
-           		//er.printStackTrace();
-           		recv = "接続失敗\n";
-       			System.out.print(recv);
-				break;
-       		}
-
-       		System.out.print(recv);
-		}
-       	udp.close();
-   	}
-
+        while(true){
+            try {
+                recv = udp.recvMesg();
+                udp.sendMesgln("OK");
+            }
+            catch (Exception er) {
+                //er.printStackTrace();
+                recv = "接続失敗\n";
+                System.out.print(recv);
+                break;
+            }
+            System.out.print(recv);
+        }
+        udp.close();
+    }
 }
