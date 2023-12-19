48dd26eba4be1799d4e93a8c2b2ff13ad2e7ed35
root <iseki>
2023-12-19 22:02:38 +0900

    change source code

--- UDP_C.java
+++ UDP_C.java
@@ -1,28 +1,23 @@
 
-
 import jp.jbxl.*;
-
-
 
 class UDP_C
 {
+    public static void  main(String args[])
+    {
+        UDP udp = new UDP("202.26.159.139", 8000);
+        String recv = "";
 
-   public static void  main(String args[])
-   {
-       UDP udp = new UDP("202.26.159.139", 8000);
-       String recv = "";
+        try {
+            udp.sendMesgln("OK");
+            recv = udp.recvMesg();
+        }
+        catch (Exception er) {
+            //er.printStackTrace();
+            recv = "接続失敗\n";
+        }
 
-       try {
-           udp.sendMesgln("OK");
-           recv = udp.recvMesg();
-       }
-       catch (Exception er) {
-           //er.printStackTrace();
-           recv = "接続失敗\n";
-       }
-
-       System.out.print(recv);
-       udp.close();
-   }
-
+        System.out.print(recv);
+        udp.close();
+    }
 }
